4f0bcd99a99a88e07f2e9da4474b608cb28fcdbf
HinsyLasong <it.hinsyuon@gmail.com>
2025-08-29 18:12:24 +0700

    Add Config Database Password

--- config/database.php
+++ config/database.php
@@ -4,4 +4,4 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'db_course_api');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'IT@1994');
+define('DB_PASSWORD', '');
